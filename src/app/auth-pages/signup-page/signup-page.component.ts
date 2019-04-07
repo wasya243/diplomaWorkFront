3f4c428c094a858974a71cc7c8f3c21700dd5d6f
Vasyl Kharhenko <v.kharchenko@mozidev.com>
2019-04-07 11:50:37 +0300

    Modify sign up page by adding a select with faculties.

--- src/app/auth-pages/signup-page/signup-page.component.ts
+++ src/app/auth-pages/signup-page/signup-page.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 import { AuthService } from '../../auth/auth.service';
 import { RoleService } from '../../core/role.service';
 import { ValidationService } from '../../core/validation.service';
 
+import IFaculty = diploma.IFaculty;
+import IProcessedFaculty = diploma.IProcessedFaculty;
 import ISignUpCredentials = diploma.ISignUpCredentials;
 import IRole = diploma.IRole;
 
@@ -33,18 +36,21 @@ export class SignupPageComponent implements OnInit {
   signupForm: FormGroup;
   currentState = passwordFieldStates.hidePassword;
   errorMessage = null;
+  faculties: Array<IProcessedFaculty> = [];
   private dispatcherRole: IRole = null;
 
   constructor(
     fb: FormBuilder,
     private authService: AuthService,
     private roleService: RoleService,
+    private http: HttpClient,
     private router: Router
   ) {
     this.signupForm = fb.group({
       firstName: [ '', [ Validators.required ] ],
       lastName: [ '', [ Validators.required ] ],
       email: [ '', [ Validators.required, ValidationService.emailValidator ] ],
+      facultyId: [ '', [ Validators.required ] ],
       password: [ '', [ Validators.required, Validators.minLength(4), Validators.maxLength(10) ] ]
     });
   }
@@ -52,13 +58,8 @@ export class SignupPageComponent implements OnInit {
   ngOnInit() {
     // TODO: check if I really need it
     this.authService.isAuthorized() && this.router.navigate([ 'admin' ]);
-    this.roleService.getRoles()
-      .subscribe((roles: IRole[]) => {
-        this.dispatcherRole = (roles.filter(role => role.name === 'dispatcher')[ 0 ]) as IRole;
-      }, (error: Error) => {
-        // TODO: think of handling this error
-        console.error(error);
-      });
+    this.getFaculties();
+    this.getRoles();
   }
 
   private canSignUp(): boolean {
@@ -83,7 +84,7 @@ export class SignupPageComponent implements OnInit {
     this.signupForm.controls.password.markAsTouched();
     if (this.canSignUp()) {
       // TODO: fetch dispatcher role instead of hardcoding it
-      const signUpData: ISignUpCredentials = Object.assign(this.formValue, { role: this.dispatcherRole.id });
+      const signUpData: ISignUpCredentials = Object.assign(this.formValue, { roleId: this.dispatcherRole.id });
       this.authService.signup(signUpData)
         .subscribe(resp => {
             this.router.navigate([ 'login' ]);
@@ -94,6 +95,27 @@ export class SignupPageComponent implements OnInit {
             }
           });
     }
+  }
+
+  private getRoles(): void {
+    this.roleService.getRoles()
+      .subscribe((roles: IRole[]) => {
+        this.dispatcherRole = (roles.filter(role => role.name === 'dispatcher')[ 0 ]) as IRole;
+      }, (error: Error) => {
+        // TODO: think of handling this error
+        console.error(error);
+      });
+  }
+
+  private getFaculties(): void {
+    // unfortunately I don't know how to make facultiesService accessible from admin module without importing it to app module
+    this.http.get<IFaculty[]>(`/faculties`)
+      .subscribe((data: Array<IFaculty>) => {
+        this.faculties = data.map(faculty => Object.assign({}, { id: faculty.id, name: faculty.name }));
+      }, error => {
+        // TODO: maybe log errors?
+        console.error(error);
+      });
   }
 
 }
